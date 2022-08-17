cbf8e1ca57d07ce6a449260190445de62132d090
in flames <109429456+Ad8p@users.noreply.github.com>
2022-08-17 11:59:58 +0430

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+alert("welcom to the website!-Click on OK--خوشامدید");
+let esm,adres,shomare,pedar,tavalod;
+esm="ali ";
+adres="59 besat";
+shomare="099094250523";
+pedar="abbas";
+tavalod="1381.6.31";
+console.log(esm,adres,shomare,pedar,tavalod);
+
+
+
+
+
+
+
+
